00f6a03dda8ee9ca87b7a5f46ac2224e81c8d743
Gaëtan ANDRIANJAFY <gaetan@ibonia.mg>
2022-03-31 07:35:15 +0300

    initial commit

---
+++ src/test/java/com/popjoestar/joke/JokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.popjoestar.joke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
