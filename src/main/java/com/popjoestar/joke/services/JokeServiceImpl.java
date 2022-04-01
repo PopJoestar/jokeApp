5c7a233ae44beec8963f746c867fa424aa117c99
Gaëtan ANDRIANJAFY <gaetan@ibonia.mg>
2022-04-01 08:09:45 +0300

    Add chucknorris view. Closes #6, closes #7

--- src/main/java/com/popjoestar/joke/services/JokeServiceImpl.java
+++ src/main/java/com/popjoestar/joke/services/JokeServiceImpl.java
@@ -4,12 +4,12 @@ import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.stereotype.Service;
 
 @Service
-public class JokeServiceImpl implements JokeService {
+public class  JokeServiceImpl implements JokeService {
 
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
-        this.chuckNorrisQuotes = chuckNorrisQuotes;
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
     }
 
     @Override
